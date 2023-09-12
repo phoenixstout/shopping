0299935d1c074246a41f421c2825ac3582550543
Phoenix <psstout@gmail.com>
2023-09-12 13:48:46 -0700

    add more products and styling

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -4,21 +4,31 @@ import { CartContext } from "./CartContext";
 import { useContext, useEffect, useState } from "react";
 import Product from "./Product";
 
-const rawUrls = ["https://fakestoreapi.com/products/1"];
+const rawUrls = [
+  "https://fakestoreapi.com/products/1",
+  "https://fakestoreapi.com/products/2",
+  "https://fakestoreapi.com/products/3",
+  "https://fakestoreapi.com/products/4",
+  "https://fakestoreapi.com/products/5",
+  "https://fakestoreapi.com/products/6",
+  "https://fakestoreapi.com/products/7",
+  "https://fakestoreapi.com/products/8",
+  "https://fakestoreapi.com/products/9",
+  "https://fakestoreapi.com/products/10"
+];
 
 function Shop() {
   const { items, setItems } = useContext(CartContext);
-
 
   return (
     <>
       <Navbar />
       <h1>Da shop</h1>
-      {
-        rawUrls.map((rawUrl, index) => {
-          return(<Product rawUrl={rawUrl} key={index}/>)
-        })
-      }
+      <div className="productsWrapper">
+        {rawUrls.map((rawUrl, index) => {
+          return <Product rawUrl={rawUrl} key={index} />;
+        })}
+      </div>
     </>
   );
 }
