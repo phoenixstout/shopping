57d7fe03003da1b991a2fd4ec95b00b48a734e66
Phoenix <psstout@gmail.com>
2023-09-12 17:25:35 -0700

    add increment decrement for product adding

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,25 @@
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
+import { useState } from "react";
 
 export default function Product({ data }) {
-  const { items, setItems} = useContext(CartContext);
-
+  const { items, setItems } = useContext(CartContext);
+  const [numToAdd, setNumToAdd] = useState(0);
 
   function handleAdd() {
     setItems((items) => [...items, data]);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  function handleNum(up) {
+    if(up) setNumToAdd(numToAdd=>numToAdd+1)
+    if(!up) {
+      if(numToAdd == 0) return
+      setNumToAdd(numToAdd => numToAdd-1)
+    }
   }
 
   if (!data) {
@@ -21,7 +34,14 @@ export default function Product({ data }) {
     <>
       <div className="productWrapper">
         {data && <img className="productImg" src={data.image} />}
-        <button onClick={handleAdd}>Add</button>
+        <div className="numInput">
+          <div>{numToAdd}</div>
+          <div className="increaseDecrease">
+            <button onClick={()=> handleNum(true)}>+</button>
+            <button onClick={()=> handleNum(false)}>-</button>
+          </div>
+          <button onClick={handleAdd}>Add</button>
+        </div>
       </div>
     </>
   );
